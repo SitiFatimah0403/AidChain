5008c8698d6b482f2c2d657674739f63d5ade9cf
jkl0vain <jkl0vain@gmail.com>
2025-08-05 12:39:42 +0800

    latest smart contract,ABI, useContract

--- frontend/src/admin/isAdmin.ts
+++ frontend/src/admin/isAdmin.ts
@@ -1,15 +1,24 @@
 //Handle logic for checking if a wallet is an admin
 
 // Function to get admin addresses from environment variable
-export const getAdminAddresses = (): string[] => {
+/*export const getAdminAddresses = (): string[] => {
   const envValue = import.meta.env.VITE_ADMIN_WALLETS_FATIMAH; //fatimah je boleh tgk buat masa ni
+
   if (!envValue) return [];
   return envValue.split(',').map(addr => addr.trim().toLowerCase());
 };
-
+*/
 // Check if the given address is in the list of admin addresses
 export const isAdmin = (address?: string): boolean => {
   if (!address) return false;
   const admins = getAdminAddresses();
   return admins.includes(address.toLowerCase());
 };
+
+//TEMP FOR TESTING! 
+export const getAdminAddresses = (): string[] => {
+  const envValue = "0x13665A3363Aa1B3B0D6F9C1Cc3B6bB5Ce1DE908b"; // tester's wallet
+
+  return envValue.split(',').map(addr => addr.trim().toLowerCase());
+};
+
